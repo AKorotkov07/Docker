6ff759efcd3ab2e3063d8eb103d081b3e89f4494
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-07-13 16:26:13 +0300

    Add files via upload

---
+++ src/main/java/AuthorizationApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class AuthorizationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+}
